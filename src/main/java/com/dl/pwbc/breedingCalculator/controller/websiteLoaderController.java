64e5493d6236ceadd2a214baa45a30b4f201b6d8
avinandan013 <avinandan.gayen.13@gmail.com>
2026-08-01 05:09:23 +0530

    added an Implimentation class for the controller and also changed the generic return type of controller to a Response Type

--- src/main/java/com/dl/pwbc/breedingCalculator/controller/websiteLoaderController.java
+++ src/main/java/com/dl/pwbc/breedingCalculator/controller/websiteLoaderController.java
@@ -1,12 +1,20 @@
 package com.dl.pwbc.breedingCalculator.controller;
 
+import com.dl.pwbc.breedingCalculator.controllerImpl.websiteLoaderImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class websiteLoaderController {
+
+    @Autowired
+    websiteLoaderImpl websiteLoaderImpl;
+
     @GetMapping("/webLoader")
-    public String webLoader(){
-        return "website loader success";
+    public ResponseEntity<String> webLoader() {
+        websiteLoaderImpl.getMessage();
+        return ResponseEntity.ok().body("loaded successfully");
     }
 }
