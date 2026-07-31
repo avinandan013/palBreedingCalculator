e0d1ca8d3aa8589349c09fef49c08a10112677f7
avinandan013 <avinandan.gayen.13@gmail.com>
2026-08-01 03:53:08 +0530

    First commit

---
+++ src/main/java/com/dl/pwbc/breedingCalculator/BreedingCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.dl.pwbc.breedingCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreedingCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreedingCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dl/pwbc/breedingCalculator/BreedingCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dl.pwbc.breedingCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreedingCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
