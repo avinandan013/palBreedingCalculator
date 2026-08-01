47e7de0a7659e44064232e0626c6f9cd3134b8bb
avinandan013 <avinandan.gayen.13@gmail.com>
2026-08-02 01:03:52 +0530

    testing out the ruleset

--- src/main/java/com/dl/pwbc/breedingCalculator/entity/Pal.java
+++ src/main/java/com/dl/pwbc/breedingCalculator/entity/Pal.java
@@ -1,5 +1,13 @@
 package com.dl.pwbc.breedingCalculator.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pal {
+    private String palName;
+    private String palGender;
+    private int palId;
 
 }
