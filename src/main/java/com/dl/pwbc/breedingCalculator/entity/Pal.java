f99cc220b6a4fdace3f5bb1f415d6e00d7740748
Prasad Ranjan Sikdar <prasadranjansikdar11@gmail.com>
2026-08-02 01:31:34 +0530

    Added service class logic and added getter setter in Pal class

--- src/main/java/com/dl/pwbc/breedingCalculator/entity/Pal.java
+++ src/main/java/com/dl/pwbc/breedingCalculator/entity/Pal.java
@@ -1,10 +1,14 @@
 package com.dl.pwbc.breedingCalculator.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Pal {
     private String palName;
     private String palGender;
