e45cb7f4f956a4942f9fa23488e9c51f984913f9
avinandan013 <avinandan.gayen.13@gmail.com>
2026-08-04 02:40:53 +0530

    Adding a new controller to get all the possible parents which works from 001 to 025 pals

--- src/main/java/com/dl/pwbc/breedingCalculator/controller/websiteLoaderController.java
+++ src/main/java/com/dl/pwbc/breedingCalculator/controller/websiteLoaderController.java
@@ -1,20 +1,33 @@
 package com.dl.pwbc.breedingCalculator.controller;
 
 import com.dl.pwbc.breedingCalculator.controllerImpl.websiteLoaderImpl;
+import com.dl.pwbc.breedingCalculator.entity.BreedingCombo;
+import com.dl.pwbc.breedingCalculator.service.WhereIsMyChild;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class websiteLoaderController {
 
     @Autowired
     websiteLoaderImpl websiteLoaderImpl;
+    @Autowired
+    WhereIsMyChild whereIsMyChild;
 
     @GetMapping("/webLoader")
     public ResponseEntity<String> webLoader() {
         websiteLoaderImpl.getMessage();
         return ResponseEntity.ok().body("loaded successfully");
+    }
+
+//    <-- example -->
+    @GetMapping("/{id}/combos")
+    public List<BreedingCombo> getCombos(@PathVariable String id) {
+        return whereIsMyChild.getCombosForOffspring(id);
     }
 }
--- src/main/java/com/dl/pwbc/breedingCalculator/service/WhereIsMyChild.java
+++ src/main/java/com/dl/pwbc/breedingCalculator/service/WhereIsMyChild.java
@@ -1,17 +1,30 @@
 package com.dl.pwbc.breedingCalculator.service;
 
+import com.dl.pwbc.breedingCalculator.Repo.BreedingComboRepository;
 import com.dl.pwbc.breedingCalculator.Repo.palFinderRepo;
+import com.dl.pwbc.breedingCalculator.entity.BreedingCombo;
 import com.dl.pwbc.breedingCalculator.entity.Pal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class WhereIsMyChild {
 
     @Autowired
     palFinderRepo pfRepo;
+    @Autowired
+    BreedingComboRepository breedingComboRepository;
     public Pal findMyChild(Pal parentA, Pal parentB){
 //        String avg = (parentA.getPalId()+parentB.getPalId()) / 2;
 //        Pal child = pfRepo.findById(avg);
 //        return child;
         return parentA;   //This one is written just to omit the error
+    }
+
+    //    <-- example -->
+    public List<BreedingCombo> getCombosForOffspring(String childId) {
+        return breedingComboRepository.findByChildId(childId);
     }
 }
