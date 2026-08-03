5607666cc0827f0ba089adbaefe31b3a33a4c13c
avinandan013 <avinandan.gayen.13@gmail.com>
2026-08-04 02:26:41 +0530

    Adding two new methods in BreedingComboRepository.java to elaborate how it works

--- src/main/java/com/dl/pwbc/breedingCalculator/Repo/BreedingComboRepository.java
+++ src/main/java/com/dl/pwbc/breedingCalculator/Repo/BreedingComboRepository.java
@@ -3,5 +3,13 @@ package com.dl.pwbc.breedingCalculator.Repo;
 import com.dl.pwbc.breedingCalculator.entity.BreedingCombo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BreedingComboRepository extends JpaRepository<BreedingCombo, Long> {
+    // Spring automatically translates this into:
+    // SELECT * FROM BREEDING_COMBOS WHERE CHILD_ID = ?
+    List<BreedingCombo> findByChildId(String childId);
+
+    // Find combos where a specific Pal is one of the parents
+    List<BreedingCombo> findByParent1IdOrParent2Id(String parent1, String parent2);
 }
